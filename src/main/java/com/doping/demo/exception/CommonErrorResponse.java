4d6e19273aef5ba890cd0b9891f641d655670ede
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-10-08 10:44:26 +0300

    common error response created

---
+++ src/main/java/com/doping/demo/exception/CommonErrorResponse.java
@@ -0,0 +1,19 @@
+package com.doping.demo.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CommonErrorResponse {
+
+     private Integer status;
+     private String message;
+     private LocalDateTime timestamp;
+
+
+}
