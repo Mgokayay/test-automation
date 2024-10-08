0109d5178e9f544b85519fbbd701b6eeef74a623
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-10-08 10:48:36 +0300

    readme created

---
+++ src/test/java/com/doping/demo/DopingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doping.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
