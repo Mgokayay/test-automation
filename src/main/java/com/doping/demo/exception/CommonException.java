c214c1e07d59a0ce47b0ab520060911978d7fafb
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-10-08 10:44:13 +0300

    common exception created

---
+++ src/main/java/com/doping/demo/exception/CommonException.java
@@ -0,0 +1,16 @@
+package com.doping.demo.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CommonException extends RuntimeException {
+
+    private HttpStatus httpStatus;
+
+    public CommonException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+}
